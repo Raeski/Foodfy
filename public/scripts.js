6f306efb680d75c8586c8610a48e67dc28b4f854
Gustavo Raeski <gustavoraeski@outlook.com>
2020-06-30 14:57:54 -0300

    Começando a adicionar o evento de esconder as receitas

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,19 @@
 const cards = document.querySelectorAll('.cards');
 
+
 for(let card of cards){
     card.addEventListener('click', () => {
         const recipeId = card.getAttribute('id');
         window.location.href = `/recipes/${recipeId}`
-        console.log(recipeId)
     });
 }
+
+const show = document.querySelectorAll('.show_button')
+const selector = document.querySelector('.information')
+
+for (let bottom of show) {
+    bottom.addEventListener("click", function(){
+        selector.classList.add('hidden')
+        location.reload(false)
+    })
+} 
