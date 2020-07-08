ee3b3931638837dcdc97ee57ec1986a0a927d04e
Gustavo Raeski <gustavoraeski@outlook.com>
2020-07-08 08:59:23 -0300

    Update mostrar/esconder

--- public/scripts.js
+++ public/scripts.js
@@ -11,13 +11,25 @@ const testes = document.querySelectorAll('.information')
 const showIng = document.querySelectorAll('.collapsible')
 
 
-for(let test of testes )
+/*for(let test of testes )
     test.addEventListener('click', function (){
         if(showIng.innerHTML == 'ESCONDER') {
             test.classList.add('active');
-            showIng.innerHTML = 'Mostrar';
+            showIng.innerHTML == 'Mostrar';
         } else {
             test.classList.remove('active');
             showIng.innerHTML = 'ESCONDER';
     }
 });
+*/
+for (let i =0; i < showIng.length; i++) {
+    showIng[i].addEventListener('click', () => {
+        if(testes[i]. classList.contains('active')) {
+            testes[i].classList.remove('active')
+            showIng[i].textContent = "ESCONDER"
+        } else {
+            testes[i].classList.add('active')
+            showIng[i].textContent = "MOSTRAR"
+        }
+    })
+}
