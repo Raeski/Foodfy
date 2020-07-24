5a4e75551c57414f66c2c618cf68bd09f7b91059
Gustavo Raeski <gustavoraeski@outlook.com>
2020-07-23 21:53:18 -0300

    Criando pagina para criar receitas

---
+++ routes.js
@@ -0,0 +1,30 @@
+const recipes = require("./data")
+const express = require('express') 
+const routes = express.Router()
+
+routes.get("/", function(req, res) { //Renderizando página index
+    return res.render("index", {items: recipes})
+})
+routes.get("/about", function(req, res) {//Renderizando página about
+    return res.render("about")
+})
+routes.get("/recipe", function(req, res){
+    return res.render("recipe", {items: recipes})
+})
+routes.get("/recipes/:index", function (req,res) {
+    const recipeIndex= req.params.index
+    const recipe = [...recipes]
+
+    return res.render('recipe', {recipe: recipe[recipeIndex -1]})
+})
+routes.get("/recipes", function(req, res) {
+    return res.render("recipes", {items: recipes})
+})
+
+
+routes.get("/admin", function(req,res) {
+    return res.render('admin/admin')
+})
+
+
+module.exports = routes
--- server.js
+++ server.js
@@ -1,46 +1,22 @@
 const express= require('express')
 const nunjucks = require('nunjucks')
-
+const routes = require('./routes')
 
 const server = express()
-const recipes = require("./data")
+
 
 server.use(express.static('public'))
-
 server.set("view engine", "njk")
+server.use(routes)
 
-nunjucks.configure("views", { //Configurando nunjucks
-    express: server
+
+nunjucks.configure("views",{ //Configurando nunjucks
+    express: server,
+    autoescape:false,
+    noCache: true
+
 })
 
-server.get("/", function(req, res) { //Renderizando página index
-    return res.render("index", {items: recipes})
-})
-
-server.get("/about", function(req, res) {//Renderizando página about
-    return res.render("about")
-})
-
-//server.get("/recipes/:index", function (req,res) {
- //   const receipts = data; //Array de receitas carregado do data.js
-  //  const recipeIndex = req.params.index;
-   // console.log(receipts[recipeIndex]);
-    //return res.render("recipe"), {recipes: data};
-//})
-server.get("/recipe", function(req, res){
-    return res.render("recipe", {items: recipes})
-})
-
-server.get("/recipes/:index", function (req,res) {
-    const recipeIndex= req.params.index
-    const recipe = [...recipes]
-
-    return res.render('recipe', {recipe: recipe[recipeIndex -1]})
-})
-
-server.get("/recipes", function(req, res) {//Renderizando página recipes
-    return res.render("recipes", {items: recipes})
-})
 
 server.listen(2000, function (){
     console.log("server is running")
